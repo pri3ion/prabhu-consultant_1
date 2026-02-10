61a0ff4ee8a37b5fefffd2c0be0fa4054b612917
Prince Kumar <102850483+pri3ion@users.noreply.github.com>
2026-02-10 18:28:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", () => {
+  document.getElementById("loader").style.display = "none";
+});
+
+function openModal() {
+  document.getElementById("projectModal").style.display = "flex";
+}
+
+function closeModal() {
+  document.getElementById("projectModal").style.display = "none";
+}
